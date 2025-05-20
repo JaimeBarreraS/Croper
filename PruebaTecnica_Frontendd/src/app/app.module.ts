de849e6a840cc223bdb380af5a5faef2ceb3199b
JaimeBarreraS <Jaenba10@gmail.com>
2025-05-19 20:44:41 -0500

    refactoring: :recycle: remove authentication components and unused products, services and status management. Restart everything due to PC problems.

--- PruebaTecnica_Frontendd/src/app/app.module.ts
+++ PruebaTecnica_Frontendd/src/app/app.module.ts
@@ -1,34 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-import { ProductEffects } from './products/store/product.effects';
-import { productReducer } from './products/store/product.reducer';
-import { ProductListComponent } from './products/components/product-list/product-list.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        ProductListComponent
+        AppComponent
     ],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        StoreModule.forRoot({ products: productReducer }),
-        EffectsModule.forRoot([ProductEffects]),
-        StoreDevtoolsModule.instrument({
-            maxAge: 25, // Retiene las últimas 25 acciones
-            logOnly: environment.production, // Restricción para prod
-        })
+        BrowserModule
+        
     ],
     providers: [],
     bootstrap: [AppComponent]
--- PruebaTecnica_Frontendd/src/app/products/models/product.model.ts
+++
@@ -1,8 +0,0 @@
-// product.model.ts
-export interface Product {
-    id?: number;
-    name: string;
-    description: string;
-    price: number;
-    stock: number;
-}
